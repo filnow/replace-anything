04e7d8461b638d47f174f09b6d6fdb0ebb05eda2
Kowalskiexe <kowalski.exe@gmail.com>
2023-04-11 00:54:41 +0200

    fix image highlighting

--- static/js/click.js
+++ static/js/click.js
@@ -1,34 +1,27 @@
 window.onload = function() {
     var img = document.getElementById("image");
-  
-    img.onclick = function(e) {
+    img.dataset.src = img.src;
+
+    img.onclick = async (e) => {
         var x = e.offsetX;
         var y = e.offsetY;
-        var width = img.clientWidth;
-        var height = img.clientHeight;
-        var image_path = document.getElementById("image").src;
+        var width = e.target.clientWidth;
+        var height = e.target.clientHeight;
+        var image_path = e.target.dataset.src;
         console.log("Clicked at: (" + x + ", " + y + ")");
         var data = { "x": x, "y": y, "image_path": image_path, "width": width, "height": height };
-        fetch('/get_coords', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
-        }).then(function(response) {
-            return response.arrayBuffer();
-        }).then(function(buffer) {
-            // Create a new blob object from the buffer data
-            var blob = new Blob([buffer], { type: 'image/jpeg' });
-        
-            // Create an image element and set its src attribute to the blob data URL
-            var img = new Image();
-            img.src = URL.createObjectURL(blob);
-        
-            // Replace the original image with the modified image
-            var oldImg = document.getElementById('image');
-            oldImg.parentNode.replaceChild(img, oldImg);
+        var response = await fetch('/get_coords', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
         });
-        
+        var buffer = await response.arrayBuffer();
+
+        // Create a new blob object from the buffer data
+        var blob = new Blob([buffer], { type: 'image/jpeg' });
+        // Replace src with the new one
+        e.target.src = URL.createObjectURL(blob);
     }
-  }
+}
